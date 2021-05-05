d8522ed548e9e3d4d49bf521e402d4862bdaf161
Astha <70913929+asthacoder@users.noreply.github.com>
2021-05-05 23:46:50 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,46 @@
+const dictionary = {
+ "hello" : {
+    "word" : "hello",
+    "lexicalCategory":"Interjection",
+    "definition" : "used as a greeting or to begin a telephone conversation"
+  },
+ 
+  "she" : {
+    "word" : "she",
+    "lexicalCategory":"Pronoun",
+    "definition" : "used to refer to a woman, girl, or female animal previously mentioned or easily identified"
+  },
+ 
+  "wonderful" : {
+    "word" : "wonderful",
+    "lexicalCategory":"Adjective",
+    "definition" : "inspiring delight, pleasure, or admiration; extremely good; marvellous"
+  },
+  "ordinary" : {
+    "word" : "ordinary",
+    "lexicalCategory":"Adjective",
+    "definition" : "with no special or distinctive features; normal"
+  },
+  "possible": {
+    "word": "possible",
+    "lexicalCategory":"Adjective",
+    "definition":"able to be done or achieved"
+  },
+  "whitehatjr":{
+    "word":"whitehatjr",
+    "lexicalCategory":"Noun",
+     "definition":"Its is most popular coding brand by karan bajaj Sir."
+  },
+  "virus":{
+    "word":"virus",
+    "lexicalCategory":"Noun",
+     "definition":"an infective agent that typically consists of a nucleic acid molecule in a protein coat, is too small to be seen by light microscopy, and is able to multiply only within the living cells of a host."
+  },
+  "coding":{
+    "word":"coding",
+     "lexicalCategory":"Noun",
+     "definition":"the process of assigning a code to something for classification or identification,the process or activity of writing computer programs"
+  }
+}
+
+export default dictionary
